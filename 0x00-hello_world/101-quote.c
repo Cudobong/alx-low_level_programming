5c6574f1230e2c807d5f0567b3e169c726a36f56
Cudobong <christianekpo940@gmail.com>
2023-02-17 10:44:27 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,9 @@
-#include<stdio.h>
-#include<unistd.h>
-
+#include <stdio.h>
+#include <unistd.h>
 /**
  * main - prints the piece of art is useeful
  * return - 1 (when successful)
 */
-
 int main(void)
 {	fput("and the piece of art is useful\" - Dora Kopar, 2015-10-19/n", stdout);
 	return (1);
