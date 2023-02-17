4159924d5e08a7e7169c8f71a0576a78e1c89d62
Cudobong <christianekpo940@gmail.com>
2023-02-17 10:56:19 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
@@ -7,7 +8,7 @@
 
 int main(void)
 {
-  char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-  write(2, msg, sizeof(msg) - 1);
-  return 1;
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, msg, sizeof(msg) - 1);
+	return 1;
 }
