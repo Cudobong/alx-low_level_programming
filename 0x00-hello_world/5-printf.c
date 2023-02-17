534e1812a5f4042f2fa96c04d870c91da8bfafc2
Cudobong <christianekpo940@gmail.com>
2023-02-17 00:14:41 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - Entry 
- * Return: Always return 0
+ * main - Use the function printf
+ * Return: 0
  */
+
 int main(void)
 {
-	printf("%s", "with proper grammar, but the outcome is a piece of art \n")
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-
-]
+}
