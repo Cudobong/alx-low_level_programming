60e67cf23d92ec59c5908dfe52e6c8d87a70915f
Cudobong <christianekpo940@gmail.com>
2023-02-17 11:00:49 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - prints the piece of art is useeful
  * return - 1 (when successful)
 */
-
 int main(void)
 {
 	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, msg, sizeof(msg) - 1);
-	return 1;
+	return (1);
 }
