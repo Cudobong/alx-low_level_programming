f6598ed0e3b83371f3d3bde5140fdef37790a193
Cudobong <christianekpo940@gmail.com>
2023-02-18 22:36:40 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
-* main - Entry point
-*
-* Return: Always 1 (Error)
-*/
-
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
 int main(void)
 {
-        char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-        write(2, msg, sizeof(msg) - 1);
-        return (1);
+    char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+    write(2, msg, sizeof(msg) - 1);
+    return (1);
 }
