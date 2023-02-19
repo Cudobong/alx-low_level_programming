d2c981d0064a50203a8164b30f166b083c02d6ed
Cudobong <christianekpo940@gmail.com>
2023-02-18 22:29:19 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-#include <unistd.h>
+#include <unistd.>
 
 /**
- * main - Entry point
- *
- * Return: Always 1 (Error)
- */
+* main - Entry point
+*
+* Return: Always 1 (Error)
+*/
+
 int main(void)
 {
         char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
