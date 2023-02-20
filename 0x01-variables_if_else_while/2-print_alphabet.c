44524e00308c5c2594963f7dbe56845e83f3520b
Cudobong <christianekpo940@gmail.com>
2023-02-20 02:47:27 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabets in lowercase
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	while
+	(c <= 'z');
+	{
+		putchar('c');
+
+		c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
