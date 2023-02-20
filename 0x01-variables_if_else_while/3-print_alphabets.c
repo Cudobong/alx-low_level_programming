db49963de8b6edb441f67174d8121222b1834240
Cudobong <christianekpo940@gmail.com>
2023-02-20 04:42:25 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - print bothe upper and lower case
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	char A;
+
+	c = 'a';
+
+	A = 'A';
+
+	while (a <= z)
+	{
+	putchar(c);
+	c++;
+	}
+
+	while (A <= Z)
+	{
+	putchar(A);
+	c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
