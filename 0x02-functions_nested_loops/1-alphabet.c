617a1cb5ced9e3fe1bcf3205dec120cdc3968436
Cudobong <christianekpo940@gmail.com>
2023-02-21 09:13:56 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry
+ * return: Always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	char z;
+
+	for (z = 'a'; z <= 'z'; z++)
+
+	{
+	_putchar(z);
+	}
+	_putchar('\n');
+
+}
