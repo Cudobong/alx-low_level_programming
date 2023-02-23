f3faea6a55aee1438186b39c391f2b4025aa9441
Cudobong <christianekpo940@gmail.com>
2023-02-23 05:52:55 -0800

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,14 +16,17 @@ int largest_number(int a, int b, int c)
 	{
 	largest = a;
 	}
+
 	else if (b >= a && a >= c)
 	{
-        largest = b;
+	largest = b;
 	}
+
 	else
 	{
 	largest = c;
 	}
 
-       	return (largest);
+	return (largest);
+
 }
